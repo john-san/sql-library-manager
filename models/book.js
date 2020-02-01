98514b2bf4a76560e147b0f7b3595cf352598c80
John Nguyen <nguyen.johnsan@gmail.com>
2020-02-01 07:00:45 -0800

    add validation to genre and year fields

--- models/book.js
+++ models/book.js
@@ -28,8 +28,41 @@ module.exports = (sequelize) => {
         }
       }
     },
-    genre: Sequelize.STRING,
-    year: Sequelize.INTEGER
+    genre: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for genre'
+        }, 
+        notEmpty: {
+          msg: 'Genre is required'
+        }
+      }
+    },
+    year: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for year'
+        }, 
+        notEmpty: {
+          msg: 'Year is required'
+        },
+        isInt: {
+          msg: 'Please enter the year in numeric characters'
+        },
+        min: {
+          args: 1,
+          msg: 'Please enter a year above 0'
+        },
+        max: {
+         args: new Date().getFullYear(),
+         msg: `Please enter a year that does not pass ${new Date().getFullYear()}`
+        }
+      }
+    },
   }, { sequelize });
 
   return Book;
